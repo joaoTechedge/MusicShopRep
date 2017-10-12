2e4634668546199f64d22c5525b6cbe90b2eb939
DESKTOP-O0P14C3\JoseBento <jose.bento@techedgegroup.com>
2017-10-12 15:52:43 +0100

    MS-005
    Angular SPA

--- musicShop/src/main/webapp/resources/app.js
+++ musicShop/src/main/webapp/resources/app.js
@@ -4,4 +4,15 @@
 
 'use strict'
 
-var App = angular.module('musicShop', []);
+var app = angular.module('musicShop', ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+
+    // route for the home page
+    .when('/', {
+        templateUrl : 'resources/album/view/album.html',
+        controller  : 'AlbumController'
+    })
+
+});
