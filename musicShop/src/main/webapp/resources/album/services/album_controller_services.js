d5065fe4547c87e2db64c72c499145a5e91145c7
DESKTOP-O0P14C3\JoseBento <jose.bento@techedgegroup.com>
2017-10-19 09:40:01 +0100

    MS-012
    CRUD album in the user interface

--- musicShop/src/main/webapp/resources/album/services/album_controller_services.js
+++ musicShop/src/main/webapp/resources/album/services/album_controller_services.js
@@ -17,13 +17,73 @@ angular.module('musicShop').factory('AlbumService', ['$http', '$q', function($ht
 			);
 
 		},
+
 		fetchAllAlbunsOrdered : function(criteria){
 			return $http.post(REST_SERVICE_URI+"/rest/album/getAlbunsFiltered", criteria)
 			.then(function (response) {
 				return response.data;
 			});
+		},
+
+		createAlbum : function (album) {
+
+			return $http({ 
+				method: 'POST', 
+				url: REST_SERVICE_URI+'/rest/album/createAlbum', 
+//				headers: {'Content-Type': 'application/json'}, 
+				data: album 
+			}).then(
+					function successCallback(response) { 
+						if (response.data) { 
+							return response.data;
+						}
+					}, 
+					function errorCallback(response) { 
+						console.log(response);
+					}
+			);			
+
+		},
+		
+		updateAlbum : function (album) {
+
+			return $http({ 
+				method: 'POST', 
+				url: REST_SERVICE_URI+'/rest/album/updateAlbum', 
+//				headers: {'Content-Type': 'application/json'}, 
+				data: album 
+			}).then(
+					function successCallback(response) { 
+						if (response.data) { 
+							return response.data;
+						}
+					}, 
+					function errorCallback(response) { 
+						console.log(response);
+					}
+			);			
+
+		},
+		
+		deleteAlbum : function (id) {
+
+			return $http({ 
+				method: 'POST', 
+				url: REST_SERVICE_URI+'/rest/album/deleteAlbum', 
+//				headers: {'Content-Type': 'application/json'}, 
+				data: id 
+			}).then(
+					function successCallback(response) { 
+						if (response.data) { 
+							return response.data;
+						}
+					}, 
+					function errorCallback(response) { 
+						console.log(response);
+					}
+			);			
+
 		}
+		
 	}
-
-
 }]);
